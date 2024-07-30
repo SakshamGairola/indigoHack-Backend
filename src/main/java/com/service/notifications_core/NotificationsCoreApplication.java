cad82b98f2ccee6d70be15b95dd6c7d871b9c5df
Saksham <saksham.gairola28@gmail.com>
2024-07-30 21:09:38 +0530

    cors allow

--- src/main/java/com/service/notifications_core/NotificationsCoreApplication.java
+++ src/main/java/com/service/notifications_core/NotificationsCoreApplication.java
@@ -12,5 +12,13 @@ public class NotificationsCoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationsCoreApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
