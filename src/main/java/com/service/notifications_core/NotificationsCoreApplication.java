849349e4e8e7278951ccf51e9966760443b89953
Saksham <saksham.gairola28@gmail.com>
2024-07-30 21:09:03 +0530

    initial commit

---
+++ src/main/java/com/service/notifications_core/NotificationsCoreApplication.java
@@ -0,0 +1,16 @@
+package com.service.notifications_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class NotificationsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsCoreApplication.class, args);
+	}
+
+}
